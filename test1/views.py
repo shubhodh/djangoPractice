0ec7502679d744e3149a94ffeed36815dbced27f
Shubhodh <shubhodh@easyecom.io>
2023-10-11 23:06:25 +0530

    changes8

--- test1/views.py
+++ test1/views.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {
-        'name': 'Hello Shubhodh'
+        'name': 'Hello Shubhodh welcome'
     })
