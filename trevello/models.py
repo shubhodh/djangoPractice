ac8ed0de80a252f7323e7caef612625f0c186ce2
Shubhodh <shubhodh@easyecom.io>
2024-03-17 19:20:35 +0530

    Models

--- trevello/models.py
+++ trevello/models.py
@@ -7,5 +7,5 @@ class Destination(models.Model):
     desc = models.CharField(max_length=200)
     name = models.TextField()
     img = models.ImageField(upload_to='pics')
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
     offer = models.BooleanField()
--- trevello/views.py
+++ trevello/views.py
@@ -4,29 +4,8 @@ from .models import Destination
 
 # Create your views here.
 def index(request):
-    dest1 = Destination()
-    dest1.name = 'Hyderabad'
-    dest1.desc = 'Get lost in heaven'
-    dest1.img = '/hyd.jpg'
-    dest1.offer = False
-    dest1.price = 9000
 
-    dest2 = Destination()
-    dest2.name = 'Bengaluru'
-    dest2.desc = 'Bengaluru makes you feel alive!'
-    dest2.img = '/bnglr.jpg'
-    dest2.offer = True
-    dest2.price = 8000
-
-    dest3 = Destination()
-    dest3.name = 'Mumbai'
-    dest3.desc = 'City that never sleeps'
-    dest3.offer = False
-    dest3.img = '/mum.jpg'
-    dest3.price = 8500
-
-    dests = [dest1, dest2, dest3]
-
+    dests = Destination.objects.all()
     return render(request, 'index.html', {
         'dests': dests
     })
